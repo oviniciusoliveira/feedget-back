99584555970ab63dedd409fcb8d291d56fe5aed3
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-05-04 14:04:14 -0300

    feat: config prisma

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (req, res) => {
+  return res.send("Hello Feedback-Get");
+});
+
+app.listen(3333, () => console.log("Server started on port 3333"));
