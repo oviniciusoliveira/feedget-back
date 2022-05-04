34e20b1e1245d3653c9b9d8e7e3b4f0043076794
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-05-04 17:36:02 -0300

    test: create SubmitFeedbackUseCase unit tests

--- src/application/useCases/SubmitFeedbackUseCase.spec.ts
+++ src/application/useCases/SubmitFeedbackUseCase.spec.ts
@@ -1,5 +1,57 @@
-describe("#SubmitFeedbackUseCase", () => {
-  test("sum 2 + 2 ", () => {
-    expect(2 + 2).toBe(4);
+import { SubmitFeedbackUseCase } from "./SubmitFeedbackUseCase";
+
+const createFeedbackMock = jest.fn();
+const sendMailMock = jest.fn();
+
+const submitFeedback = new SubmitFeedbackUseCase(
+  { create: createFeedbackMock },
+  { sendMail: sendMailMock }
+);
+
+describe("#Submit Feedback", () => {
+  it("should be able to submit a feedback", async () => {
+    await expect(
+      submitFeedback.perform({
+        type: "any_type",
+        comment: "any_comment",
+        screenshot: "data:image/png;base64,any_string",
+      })
+    ).resolves.not.toThrow();
+
+    expect(createFeedbackMock).toHaveBeenCalled();
+    expect(createFeedbackMock).toHaveBeenCalledTimes(1);
+
+    expect(sendMailMock).toHaveBeenCalled();
+    expect(sendMailMock).toHaveBeenCalledTimes(1);
+  });
+
+  it("should not be able to submit a feedback without type", async () => {
+    await expect(
+      submitFeedback.perform({
+        type: "",
+        comment: "any_comment",
+        screenshot: "data:image/png;base64,any_string",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback without comment", async () => {
+    await expect(
+      submitFeedback.perform({
+        type: "any_type",
+        comment: "",
+        screenshot: "data:image/png;base64,any_string",
+      })
+    ).rejects.toThrow();
+  });
+
+  it("should not be able to submit a feedback with an invalid screenshot", async () => {
+    await expect(
+      submitFeedback.perform({
+        type: "any_type",
+        comment: "any_comment",
+        screenshot: "invalid_screenshot",
+      })
+    ).rejects.toThrow();
   });
 });
