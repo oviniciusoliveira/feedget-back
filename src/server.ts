ac18a75e045f7628386060292aebc8d56590f7ed
Vinicius Oliveira <viniciuso.contato@gmail.com>
2022-05-04 14:59:03 -0300

    feat: send email

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
+
 import { prisma } from "./prisma";
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7751fd3cee422c",
+    pass: "40b20dad0b077c",
+  },
+});
 
 app.post("/feedbacks", async (req, res) => {
   const { type, comment, screenshot } = req.body;
@@ -13,6 +24,19 @@ app.post("/feedbacks", async (req, res) => {
       type,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equpe Feedget <contato@feedget.com>",
+    to: "Vinícius Oliveira <viniciuso.contato@gmail.com>",
+    subject: "Novo feedback reportado",
+    html: [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+      `<h1>Novo feedback reportado</h1>`,
+      `<p>Tipo: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return res.status(201).json({ data: feedback });
